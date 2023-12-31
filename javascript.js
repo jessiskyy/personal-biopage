c8e69cb0a2fcd6a9a481a247ba98e4d0ee878486
Jessica Maddux <jessicamaddux@Jessicas-Air.lan>
2023-12-30 21:35:33 -0600

    added success.html and success.js files and css styling

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 console.log("Hello World!");
 
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (event) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
+
+
 
---
+++ success.js
@@ -0,0 +1,3 @@
+setTimeout(( SuccessfullySentContactInfo ) => {
+    window.location.href = window.location.origin
+  }, 5000)
